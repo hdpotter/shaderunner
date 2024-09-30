bfb2a3ad10059113f04cf47b68d90257f4addb6a
hdpotter <hdpotter@iastate.edu>
2024-09-30 13:56:43 -0500

    fix example

--- examples/example.rs
+++ examples/example.rs
@@ -142,6 +142,6 @@ pub fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
 
     pollster::block_on(
-        shaderunner::window::run_program::<shaderunner::example::EchoesProgram<RenderGame>>()
+        shaderunner::window::run_program::<EchoesProgram<RenderGame>>()
     );
 }
